d638b0590d070c0ba48d71d702f15ad260825403
Admin <Admin@DESKTOP-HEOO8JS>
2024-02-10 18:39:08 +0200

    folders rearrange

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php
+require_once "services/requestService.php";
+require_once "models/Product.php";
+require 'vendor/autoload.php';
+
+$id = isset($_GET['id']) ? $_GET['id'] : null;
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,12 +16,17 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-  <link rel="stylesheet" href="shared/style/cards.css">
+  <link rel="stylesheet" href="/style/cards.css">
 </head>
 
 <body>
   <div id="container">
-    <?php require_once ("shared/templates/navbar-main.php"); ?>
+    <?php require_once "./templates/navbar-products.php"; ?>
+    <div id="cards">
+      <?php require_once "./controllers/product.php" ?>
+    </div>
   </div>
+  <script src="./scripts/buttons.js"></script>
 </body>
+
 </html>
--- templates/navbar-products.php
+++ templates/navbar-products.php
@@ -1,19 +1,20 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-        <a class="navbar-brand" href="/index.php">Menu</a>
+        <a class="navbar-brand" href="/index.php">Products</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/productPart/products.php"> Products</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/userPart/users.php">Users</a>
+                    <a class="nav-link active" aria-current="page" href="/views/create.php">Create</a>
                 </li>
             </ul>
-            
+            <form id="searchForm" class="d-flex" role="search" method="post" 
+            action="/controllers/search.php">
+                <input id="searchInput" name="nameSearch" class="form-control me-2" type="search" placeholder="Search by name..." aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
         </div>
     </div>
 </nav>
